94384c3c1890fc3be5de6b13e4df96382c9b37e6
ppp-o <99623992+ppp-o@users.noreply.github.com>
2022-06-09 10:32:40 +1200

    added quit function

--- Trail.py
+++ Trail.py
@@ -3,11 +3,15 @@ from tkinter import ttk
 
 root= Tk()
 
+#quit subroutine
+def quit():
+    main_window.destroy()
+
 def print_Julies_party_hire_details():
     # these are the global variables that are used
     global j_names, total_entries, name_count
     name_count = 0
-    # Create the column headings
+    # column headings
     Label(main_window, font=("Helvetica 10 bold"),
           text="Row").grid(column=0, row=7)
     Label(main_window, font=("Helvetica 10 bold"),
@@ -17,7 +21,22 @@ def print_Julies_party_hire_details():
     Label(main_window, font=("Helvetica 10 bold"),
           text="Item hired").grid(column=3, row=7)
     Label(main_window, font=("Helvetica 10 bold"),
-          text="NUmber of iteams").grid(column=4, row=7)
+          text="Number of iteams").grid(column=4, row=7)
+
+# add each item in the list into its own row
+    while name_count < total_entries:
+        Label(main_window, text=name_count).grid(column=0, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][0])).grid(
+            column=1, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][1])).grid(
+            column=2, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][2])).grid(
+            column=3, row=name_count+8)
+        Label(main_window, text=(camp_details[name_count][3])).grid(
+            column=4, row=name_count+8)
+        name_count += 1
+
+main()
 
 
 
