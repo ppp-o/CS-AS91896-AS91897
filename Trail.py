8d56d36279f11c40fd55a82d9084e4e8b89d7ea3
ppp-o <99623992+ppp-o@users.noreply.github.com>
2022-06-08 14:59:15 +1200

    Creating text boxs

--- Trail.py
+++ Trail.py
@@ -1,7 +1,23 @@
 from tkinter import * 
 from tkinter import ttk 
 
-root= tk()
+root= Tk()
+
+def print_Julies_party_hire_details():
+    # these are the global variables that are used
+    global j_names, total_entries, name_count
+    name_count = 0
+    # Create the column headings
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="Row").grid(column=0, row=7)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="Customer full name").grid(column=1, row=7)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="Recipt number").grid(column=2, row=7)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="Item hired").grid(column=3, row=7)
+    Label(main_window, font=("Helvetica 10 bold"),
+          text="NUmber of iteams").grid(column=4, row=7)
 
 
 
