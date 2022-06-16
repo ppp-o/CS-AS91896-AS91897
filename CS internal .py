7f4de62ac7c862d6ea2bc18810c7803ed7134ac1
ppp-o <99623992+ppp-o@users.noreply.github.com>
2022-06-16 14:17:51 +1200

    Set the error messages

--- CS internal .py
+++ CS internal .py
@@ -1,8 +1,6 @@
-
-
 from tkinter import *
 
-#quit function 
+#quit subroutine
 def quit():
     main_window.destroy()
 
@@ -10,11 +8,11 @@ def quit():
 def print_store_details ():
     global total_entries, item_count
     item_count = 0
-    Label(main_window, font='bold',text="Julie's Party Hire").grid(column=0,row=7)
+    Label(main_window, font='bold',text="Row").grid(column=0,row=7)
     Label(main_window, font='bold',text="Customers Full Name").grid(column=1,row=7)
     Label(main_window, font='bold',text="Receipt Number").grid(column=2,row=7)
-    Label(main_window, font='bold',text="Item Hired").grid(column=3,row=7)
-    Label(main_window, font='bold',text="Number of Items Hired").grid(column=4,row=7)
+    Label(main_window, font='bold',text="Number of Items Hired").grid(column=3,row=7)
+    Label(main_window, font='bold',text="Item Hired").grid(column=4,row=7)
 
     while item_count < total_entries :
         Label(main_window, text=item_count).grid(column=0,row=item_count+8) 
@@ -24,8 +22,42 @@ def print_store_details ():
         Label(main_window, text=(store_details[item_count][3])).grid(column=4,row=item_count+8)
         item_count +=  1
 
-# def check_inputs():
 
+def check_inputs():
+    # these are the global variables that are used
+    global store_details, entry_customer_name, entry_receipt_number, entry_item_hired, entry_item_number, total_entries
+    input_check = 0
+    Label(main_window, text="               ") .grid(column=2, row=2)
+    Label(main_window, text="               ") .grid(column=2, row=3)
+    Label(main_window, text="               ") .grid(column=2, row=4)
+    Label(main_window, text="                     ") .grid(column=2, row=5)
+    # Check that Customers_Full_Name is not blank, set error text if blank
+    if len(entry_customer_name.get()) == 0:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=2)
+        input_check = 1
+    # Check that Item_Hired is not blank, set error text if blank
+    if len(entry_item_hired.get()) == 0:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=4)
+        input_check = 1
+    if len(entry_item_number.get()) == 0:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=5)
+        input_check = 1
+    # Check the Number_of_Items_Hired is not blank and between 1 and 500, set error text if blank
+    if (entry_item_number.get().isdigit()):
+        if int(entry_item_number.get()) < 1 or int(entry_item_number.get()) > 500:
+            Label(main_window, fg="red", text="1-500 only") .grid(column=2, row=5)
+            input_check = 1
+        else:
+            pass
+    else:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=5)
+        input_check = 1
+    # Check that Receipt Number is not blank, set error text if blank
+    if len(entry_receipt_number.get()) == 0:
+        Label(main_window, fg="red", text="Required") .grid(column=2, row=3)
+        input_check = 1
+    if input_check == 0:
+        append_details()
 
 #add the print_store_details to the list
 def append_details ():
@@ -37,7 +69,6 @@ def append_details ():
         entry_item_number.delete(0,'end')
         entry_item_hired.delete(0,'end')
         total_entries +=  1
-
 
 #deleteing a row from the list
 def delete_row ():
@@ -56,7 +87,7 @@ def delete_row ():
 def setup_buttons():
     global store_details, entry_customer_name,entry_receipt_number,entry_item_number,entry_item_hired, total_entries, delete_item
     Button(main_window, text="Quit",command=quit) .grid(column=3, row=1)
-    Button(main_window, text="Append Details",command=append_details) .grid(column=0,row=1)
+    Button(main_window, text="Append Details",command=check_inputs) .grid(column=0,row=1)
     Button(main_window, text="Print Details",command=print_store_details) .grid(column=1,row=1)
     Label(main_window, text="Customer Full Name") .grid(column=0,row=2)
     entry_customer_name = Entry(main_window)
@@ -65,20 +96,22 @@ def setup_buttons():
     entry_receipt_number = Entry(main_window)
     entry_receipt_number.grid(column=1,row=3)
     Label(main_window, text="Item Hired") .grid(column=0,row=4)
-    entry_item_number = Entry(main_window)
-    entry_item_number.grid(column=1,row=4)
-    Label(main_window, text="Number of Item/s Hired") .grid(column=0,row=5)
     entry_item_hired = Entry(main_window)
-    entry_item_hired.grid(column=1,row=5)
+    entry_item_hired.grid(column=1,row=4)
+    Label(main_window, text="Number of Item/s Hired") .grid(column=0,row=5)
+    entry_item_number = Entry(main_window)
+    entry_item_number.grid(column=1,row=5)
     Label(main_window, text="Row #") .grid(column=0,row=6)
     delete_item = Entry(main_window)
     delete_item .grid(column=1,row=6)
     Button(main_window, text="Delete row",command=delete_row) .grid(column=2,row=6)
 
+
+
 #start the program running
 def main():
     global main_window
-    global store_details, entry_name,entry_age,entry_gender, total_entries
+    global store_details, total_entries
     store_details = []
     total_entries = 0
     main_window =Tk()
@@ -87,4 +120,6 @@ def main():
     main_window.mainloop()
     
 main()
+
+
 
